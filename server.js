62d3ae6a0189a568fa1361e65839735b9fb123ae
Jonathan Gil <jhongilp@gmail.com>
2017-03-24 15:56:53 +0000

    Chage https config

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000 || 8080;
 
-app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto' === 'http']) {
-     next();   
-    } else {
-        res.redirect('http://' + req.hostname + req.url);
-    }
-});
+// app.use(function(req, res, next) {
+//     if (req.headers['x-forwarded-proto' === 'http']) {
+//      next();   
+//     } else {
+//         res.redirect('http://' + req.hostname + req.url);
+//     }
+// });
 
 app.use(express.static('public'));
 
